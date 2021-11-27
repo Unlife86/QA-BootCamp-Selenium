a583e06fadd04b821aa52219d1aeccada0256789
Unlife86 <der_schatten_ui@mail.ru>
2021-11-28 03:16:49 +0700

    FirefoxDriver ElementNotInteractableException

--- src/test/java/Task11.java
+++ src/test/java/Task11.java
@@ -2,9 +2,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,6 +31,36 @@ public class Task11 extends Testt {
         baseURL = "http://localhost/litecart/en/";
     }
 
+    private int _getIndexOptionByText(List<WebElement> options, String text) {
+        int i = 0;
+        for (WebElement option : options) {
+            if (text.equals(option.getText())) {
+                return i;
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    private void _setOption(WebElement element, String optionText) {
+        Select select;
+        select = new Select(element);
+        String js = "arguments[0].selectedIndex = " + _getIndexOptionByText(select.getOptions(),optionText) + "; arguments[0].dispatchEvent(new Event('change'))";
+        try {
+            select.selectByVisibleText(optionText);
+        } catch (ElementNotInteractableException e) {
+            ((JavascriptExecutor) driver).executeScript(js,select);
+        }
+    }
+
+    @Override
+    @BeforeAll
+    public void start() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
     @Test
     public void createNewCustomer() {
         Select country_code;
@@ -34,8 +69,7 @@ public class Task11 extends Testt {
 
         driver.get(baseURL);
         driver.findElement(By.id("box-account-login")).findElement(By.linkText("New customers click here")).click();
-        country_code = new Select(driver.findElement(By.name("customer_form")).findElement(By.name("country_code")));
-        country_code.selectByVisibleText("United States");
+        _setOption(driver.findElement(By.name("customer_form")).findElement(By.name("country_code")),"United States");
 
         for (WebElement input : driver.findElement(By.name("customer_form")).findElements(By.cssSelector("input:not([type=hidden])"))) {
             if (!"checkbox".equals(input.getAttribute("type"))) {
@@ -65,16 +99,14 @@ public class Task11 extends Testt {
 
         if ("true".equals(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")).getAttribute("disabled"))) {
             driver.findElement(By.cssSelector("button[name=create_account]")).click();
-            zone_code = new Select(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")));
-            zone_code.selectByVisibleText("District of Columbia");
+            _setOption(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")),"District of Columbia");
             driver.findElement(By.name("customer_form")).findElement(By.name("password")).sendKeys(values.get(10));
             driver.findElement(By.name("customer_form")).findElement(By.name("confirmed_password")).sendKeys(values.get(10));
-            driver.findElement(By.cssSelector("button[name=create_account]")).click();
+
         } else {
-            zone_code = new Select(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")));
-            zone_code.selectByVisibleText("District of Columbia");
-            driver.findElement(By.cssSelector("button[name=create_account]")).click();
+            _setOption(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")),"District of Columbia");
         }
+        driver.findElement(By.cssSelector("button[name=create_account]")).click();
 
         driver.findElement(By.id("box-account")).findElement(By.linkText("Logout")).click();
         driver.findElement(By.id("box-account-login")).findElement(By.name("email")).sendKeys(values.get(8));
