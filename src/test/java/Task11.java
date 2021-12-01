7391717de8d32e1f727d48aba0b2a513e10e3f88
Unlife86 <der_schatten_ui@mail.ru>
2021-12-01 16:09:20 +0700

    Task11

--- src/test/java/Task11.java
+++ src/test/java/Task11.java
@@ -1,10 +1,10 @@
+import com.google.gson.Gson;
+import net.andreinc.mockneat.MockNeat;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,9 +12,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Task11 extends Testt {
-    private List<String> values = Arrays.asList(new String[]{
+    private Map<String,String> values /*= Arrays.asList(new String[]{
             "TaxID",
             "My Company",
             "MyFirstName",
@@ -26,9 +27,30 @@ public class Task11 extends Testt {
             "test@test.com",
             "+19876543214",
             "12345"
-    });
+    })*/;
+    private MockNeat mock = MockNeat.old();
     public Task11() {
         baseURL = "http://localhost/litecart/en/";
+        values = (new Gson()).fromJson(
+                String.format(
+                        "{'company':'%s','firstname':'%s','lastname':'%s','address1':'%s','address2':'%s','postcode':'%s','city':'%s','email':'%s','phone':'%s','password':'%s','confirmed_password':'%s'}",
+                        "My Company", //company
+                        mock.names().first().get(),//firstname
+                        mock.names().last().get(),//lastname
+                        "Address1",//address1
+                        "Address2",//address2
+                        String.valueOf(mock.ints().range(10000,99999).get()),//postcode
+                        "Rockville",//city
+                        mock.emails().get(),//email
+                        "+19876543214",//phone
+                        "12345",//password
+                        "12345"//confirmed_password
+
+
+                ),
+                Map.class
+        );
+
     }
 
     private int _getIndexOptionByText(List<WebElement> options, String text) {
@@ -62,6 +84,7 @@ public class Task11 extends Testt {
     }
 
     @Test
+    @DisplayName("User registration scenario")
     public void createNewCustomer() {
         Select country_code;
         Select zone_code;
@@ -71,37 +94,19 @@ public class Task11 extends Testt {
         driver.findElement(By.id("box-account-login")).findElement(By.linkText("New customers click here")).click();
         _setOption(driver.findElement(By.name("customer_form")).findElement(By.name("country_code")),"United States");
 
-        for (WebElement input : driver.findElement(By.name("customer_form")).findElements(By.cssSelector("input:not([type=hidden])"))) {
+        for (WebElement input : driver.findElement(By.name("customer_form")).findElements(By.cssSelector("input:not([type=hidden]):not([type=checkbox]):not([name=tax_id])"))) {
             if (!"checkbox".equals(input.getAttribute("type"))) {
                 input.clear();
-            }
-            switch (input.getAttribute("type")) {
-                case "text":
-                    input.sendKeys(values.get(i));
-                    i++;
-                    break;
-                case "email":
-                    input.sendKeys(values.get(8));
-                    break;
-                case "tel":
-                    input.sendKeys(values.get(9));
-                    break;
-                case "password":
-                    input.sendKeys(values.get(10));
-                    break;
-                case "checkbox":
-                    input.click();
-                    break;
-                default:
-                    break;
+                input.sendKeys(values.get(input.getAttribute("name")));
             }
         }
+        driver.findElement(By.name("newsletter")).click();
 
         if ("true".equals(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")).getAttribute("disabled"))) {
             driver.findElement(By.cssSelector("button[name=create_account]")).click();
             _setOption(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")),"District of Columbia");
-            driver.findElement(By.name("customer_form")).findElement(By.name("password")).sendKeys(values.get(10));
-            driver.findElement(By.name("customer_form")).findElement(By.name("confirmed_password")).sendKeys(values.get(10));
+            driver.findElement(By.name("customer_form")).findElement(By.name("password")).sendKeys(values.get("password"));
+            driver.findElement(By.name("customer_form")).findElement(By.name("confirmed_password")).sendKeys(values.get("confirmed_password"));
 
         } else {
             _setOption(driver.findElement(By.name("customer_form")).findElement(By.cssSelector("select[name=zone_code]")),"District of Columbia");
@@ -109,24 +114,10 @@ public class Task11 extends Testt {
         driver.findElement(By.cssSelector("button[name=create_account]")).click();
 
         driver.findElement(By.id("box-account")).findElement(By.linkText("Logout")).click();
-        driver.findElement(By.id("box-account-login")).findElement(By.name("email")).sendKeys(values.get(8));
-        driver.findElement(By.id("box-account-login")).findElement(By.name("password")).sendKeys(values.get(10));
+        driver.findElement(By.id("box-account-login")).findElement(By.name("email")).sendKeys(values.get("email"));
+        driver.findElement(By.id("box-account-login")).findElement(By.name("password")).sendKeys(values.get("password"));
         driver.findElement(By.id("box-account-login")).findElement(By.cssSelector("button[name=login]")).click();
         driver.findElement(By.id("box-account")).findElement(By.linkText("Logout")).click();
 
-    }
-
-    @Override
-    @AfterAll
-    public void stop() {
-        driver.get("http://localhost/litecart/admin/");
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("login")).click();
-        driver.findElement(By.id("box-apps-menu")).findElement(By.linkText("Customers")).click();
-        driver.findElement(By.name("customers_form")).findElement(By.linkText(values.get(2) + " " + values.get(3))).click();
-        driver.findElement(By.name("customer_form")).findElement(By.name("delete")).click();
-        driver.switchTo().alert().accept();
-        super.stop();
     }
 }
