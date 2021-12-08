0ba0b666377790083d40dbf88010c11f698b8fbc
Unlife86 <der_schatten_ui@mail.ru>
2021-12-08 22:25:19 +0700

    Task10

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,17 +1,97 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import static  org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-
-public class Task1 extends Testt {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class Task1 /*extends Testt implements Runnable*/ {
     public Task1() {
         baseURL = "https://search.maven.org/";
     }
+    /*private Thread chrome;
+    private Thread firefox;*/
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected List<DynamicTest> dynamicTests = new ArrayList<DynamicTest>();
+    protected List<WebDriver> drivers = Arrays.asList(new ChromeDriver(), new FirefoxDriver());
+    protected List<ThreadLocalDriver> threadsLocal = new ArrayList<ThreadLocalDriver>();
+    protected List<Thread> threads = new ArrayList<Thread>();
+    protected String baseURL;
 
-    @Test
-    public void Task3() {
+    public static ThreadLocal<WebDriver> threadLocal = new ThreadLocal<WebDriver>();
 
-        driver.get(baseURL);
-        wait.until(titleIs("Maven Central Repository Search"));
+    class ThreadLocalDriver implements Runnable {
+        private volatile WebDriver driver;
+
+        public ThreadLocalDriver(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        @Override
+        public void run() {
+            threadLocal.set(driver);
+        }
+
+        public WebDriver getDriver() {
+            return driver;
+        }
+    }
+
+    @BeforeAll
+    public void start() throws InterruptedException {
+        for (WebDriver driver : drivers) {
+            threadsLocal.add(new ThreadLocalDriver(driver));
+        }
+        for (ThreadLocalDriver threadLocal : threadsLocal) {
+            threads.add(new Thread(threadLocal));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+            thread.join();
+        }
+    }
+
+    @AfterAll
+    public void stop() {
+        for (WebDriver driver : drivers) {
+            driver.quit();
+            driver = null;
+        }
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+
+    protected void  _dynamicTests() {
+        dynamicTests.add(
+                _lambaDynamicTests()
+        );
+    }
+
+    protected DynamicTest _lambaDynamicTests() {
+        return DynamicTest.dynamicTest(
+                driver.getClass().getSimpleName(),
+                () -> Assertions.assertTrue(wait.until(titleIs("Maven Central Repository Search")))
+        );
+    }
+
+    @TestFactory
+    public Collection<DynamicTest> Task() {
+        for (int i = 0; i < drivers.size(); i++) {
+            driver = threadsLocal.get(i).getDriver();
+            driver.get(baseURL);
+            wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            _dynamicTests();
+        }
+        return dynamicTests;
     }
 }
--- src/test/java/Task10.java
+++ src/test/java/Task10.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -14,48 +16,66 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Task10: The product properties")
-public class Task10 extends Testt {
+public class Task10 extends Task1 {
 
     public Task10() {
         baseURL = "http://localhost/litecart/en/";
+        List<WebDriver> drivers = Arrays.asList(
+                new ChromeDriver(),
+                new FirefoxDriver(),
+                new EdgeDriver()
+        );
     }
 
-    private DynamicTest _factory(final String index, int i) {
-        List<String> prop = Arrays.asList(new String[]{"Name","Regular price", "Campaign price"});
-        List<String> productProp = Arrays.asList(new String[]{"title","regular-price", "campaign-price"});
-        return DynamicTest.dynamicTest(prop.get(i), () -> assertEquals(
-                index,
-                driver.findElement(By.cssSelector("#box-product ." + productProp.get(i))).getText()
-        ));
-    }
-    
-    @TestFactory
-    public Collection<DynamicTest> nameTest() {
+    @Override
+    protected void _dynamicTests() {
         List<String> productPropIndex = Arrays.asList(new String[]{"name","regular-price", "campaign-price"});
         List<String> valPropProductIndex = new ArrayList<String>();
-        List<DynamicTest> dynamicTests = new ArrayList<DynamicTest>();
 
         for (int i = 0; i < 3; i++) {
             valPropProductIndex.add(driver.findElement(By.cssSelector("#box-campaigns .product:first-child ." + productPropIndex.get(i))).getText());
         }
         driver.findElement(By.cssSelector("#box-campaigns .product:first-child .link")).click();
         for (int i = 0; i < 3; i++) {
-            dynamicTests.add(_factory(valPropProductIndex.get(i),i));
+            dynamicTests.add(_lambaDynamicTests(valPropProductIndex.get(i),i));
         }
-        return dynamicTests;
     }
+
+    protected DynamicTest _lambaDynamicTests(final String index, int i) {
+        List<String> prop = Arrays.asList(new String[]{"Name","Regular price", "Campaign price"});
+        List<String> productProp = Arrays.asList(new String[]{"title","regular-price", "campaign-price"});
+        return DynamicTest.dynamicTest(
+                driver.getClass().getSimpleName()+": "+ prop.get(i),
+                () -> assertEquals(
+                    index,
+                    driver.findElement(By.cssSelector("#box-product ." + productProp.get(i))).getText()
+                )
+        );
+    }
+
+    @Override
+    @TestFactory
+    @DisplayName("Name Test")
+    public Collection<DynamicTest> Task() {
+        return super.Task();
+    }
+
     @Nested
     @DisplayName("Task10: Prices on Index page")
     class PriceIndex {
-        protected DynamicTest _style(final int i, final WebElement element) {
+        protected List<DynamicTest> dynamicTests = new ArrayList<DynamicTest>();
+        protected DynamicTest _lambaDynamicTests(final int i, final WebElement element) {
             List<String> cssAttr = Arrays.asList(new String[]{"text-decoration-line", "font-weight"});
             List<String> cssAttrVal = Arrays.asList(new String[]{"line-through", "700"});
-            return DynamicTest.dynamicTest("Style text." + element.getAttribute("class"), () -> assertEquals(
-                    cssAttrVal.get(i),
-                    element.getCssValue(cssAttr.get(i))
-            ));
+            return DynamicTest.dynamicTest(
+                    driver.getClass().getSimpleName()+": Style text." + element.getAttribute("class"),
+                    () -> assertEquals(
+                        cssAttrVal.get(i),
+                        element.getCssValue(cssAttr.get(i))
+                )
+            );
         }
-        protected DynamicTest _color(final int i, final String val, final String text) {
+        protected DynamicTest _lambaDynamicTests(final int i, final String val, final String text) {
             List<String> color = Arrays.asList(new String[]{
                     String.format(
                             "^%s,%s,%s,",
@@ -68,8 +88,56 @@ public class Task10 extends Testt {
             Pattern pattern = Pattern.compile(color.get(i));
             Matcher matcher = pattern.matcher(val);
             System.out.println(val);
-            return DynamicTest.dynamicTest("Color text."+ text, () -> assertTrue(matcher.find()));
+            return DynamicTest.dynamicTest(
+                    driver.getClass().getSimpleName()+": Color text."+ text,
+                    () -> assertTrue(matcher.find())
+            );
         }
+
+        public void _dynamicTests() {
+            List<WebElement> prices = getPrices();
+            for (int i = 0; i < 2; i++) {
+                dynamicTests.add(_lambaDynamicTests(i, prices.get(i)));
+                dynamicTests.add(_lambaDynamicTests(i,
+                        prices.get(i).getCssValue("color").replaceAll("[^0-9,]", ""),
+                        prices.get(i).getAttribute("class"))
+                );
+            }
+        }
+
+        @TestFactory
+        public Collection<DynamicTest> Task() {
+            for (int i = 0; i < drivers.size(); i++) {
+                driver = threadsLocal.get(i).getDriver();
+                _dynamicTests();
+            }
+            return dynamicTests;
+        }
+
+        protected DynamicTest _lambaDynamicTests(float regPriceTextSize, float campPriceTextSize) {
+            return DynamicTest.dynamicTest(
+                    driver.getClass().getSimpleName()+": Font-size of a price",
+                    () -> assertTrue(regPriceTextSize < campPriceTextSize)
+            );
+        }
+
+        @TestFactory
+        public Collection<DynamicTest> regularAndCampaignTextSizePrice() {
+            List<DynamicTest> dynamicTests = new ArrayList<DynamicTest>();
+            for (int i = 0; i < drivers.size(); i++) {
+                driver = threadsLocal.get(i).getDriver();
+                List<WebElement> prices = new ArrayList<WebElement>();
+                prices = getPrices();
+                dynamicTests.add(
+                        _lambaDynamicTests(
+                                Float.parseFloat(prices.get(0).getCssValue("font-size").replaceAll("[^0-9.,]", "")),
+                                Float.parseFloat(prices.get(1).getCssValue("font-size").replaceAll("[^0-9.,]", ""))
+                        )
+                );
+            }
+            return dynamicTests;
+        }
+
         protected List<WebElement> getPrices() {
             List<WebElement> prices = new ArrayList<WebElement>();
             if (driver.getCurrentUrl() != Task10.this.baseURL) {
@@ -78,32 +146,6 @@ public class Task10 extends Testt {
             prices.add(driver.findElement(By.cssSelector("#box-campaigns .product:first-child .regular-price")));
             prices.add(driver.findElement(By.cssSelector("#box-campaigns .product:first-child .campaign-price")));
             return prices;
-        }
-        @TestFactory
-        public Collection<DynamicTest> priceTest() {
-            List<DynamicTest> dynamicTests = new ArrayList<DynamicTest>();
-            List<WebElement> prices = getPrices();
-            for (int i = 0; i < 2; i++) {
-                dynamicTests.add(_style(i, prices.get(i)));
-                dynamicTests.add(_color(i,
-                        prices.get(i).getCssValue("color").replaceAll("[^0-9,]", ""),
-                        prices.get(i).getAttribute("class"))
-                );
-            }
-
-            return dynamicTests;
-        }
-
-        @Test
-        public void regularAndCampaignTextSizePrice() {
-            List<WebElement> prices = new ArrayList<WebElement>();
-            prices = getPrices();
-            float regPriceTextSize = Float.parseFloat(prices.get(0).getCssValue("font-size").replaceAll("[^0-9.,]", ""));
-            float campPriceTextSize = Float.parseFloat(prices.get(1).getCssValue("font-size").replaceAll("[^0-9.,]", ""));
-            if (Task10.this.baseURL.equals(driver.getCurrentUrl())) {
-                System.out.println("Hi");
-            }
-            assertTrue(regPriceTextSize < campPriceTextSize);
         }
     }
 
